f0a70f35f36cce0605cca958e933fda235e1cf06
saemileee <68241138+saemileee@users.noreply.github.com>
2023-02-06 22:51:48 +0900

    23-02-06 22:51 edit mode

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 const todoInputElem = document.querySelector(".todo-input");
 const todoListElem = document.querySelector(".todo-list");
 
-const completeTodo = (todoId) => {
-  const newTodos = getAllTodos().map((todo) =>
-    todo.id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
-  );
-  setTodos(newTodos);
-  paintTodos();
-};
-
-const deleteTodo = (todoId) => {
-  const newTodos = getAllTodos().filter((todo) => todo.id !== todoId);
-  setTodos(newTodos);
-  paintTodos();
-};
-
 let todos = [];
 let id = 0;
 
@@ -48,8 +34,57 @@ const appendTodos = (text) => {
   paintTodos();
 };
 
+const completeTodo = (todoId) => {
+  const newTodos = getAllTodos().map((todo) =>
+    todo.id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
+  );
+  setTodos(newTodos);
+  paintTodos();
+};
+
+const deleteTodo = (todoId) => {
+  const newTodos = getAllTodos().filter((todo) => todo.id !== todoId);
+  setTodos(newTodos);
+  paintTodos();
+};
+
+const onDblclickTodo = (e, todoId) => {
+  console.log(e);
+  const todoElem = e.target;
+  const inputText = e.target.innerText;
+  const todoItemElem = todoElem.parentNode;
+  const inputElem = document.createElement("input");
+  inputElem.value = inputText;
+  inputElem.classList.add("edit-input");
+
+  inputElem.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      updateTodo(e.target.value, todoId);
+      document.body.removeEventListener("click", onClickBody);
+    }
+  });
+
+  const onClickBody = (e) => {
+    if (e.target !== inputElem) {
+      todoItemElem.removeChild(inputElem);
+      document.body.removeEventListener("click", onClickBody);
+    }
+  };
+
+  document.body.addEventListener("click", onClickBody);
+  todoItemElem.appendChild(inputElem);
+};
+
+const updateTodo = (text, todoId) => {
+  const newTodos = getAllTodos().map((todo) =>
+    todo.id === todoId ? { ...todo, content: text } : todo
+  );
+  setTodos(newTodos);
+  paintTodos();
+};
+
 const paintTodos = () => {
-  todoListElem.innerHTML = null;
+  todoListElem.innerHTML = "";
   const allTodos = getAllTodos();
 
   allTodos.forEach((todo) => {
@@ -63,6 +98,9 @@ const paintTodos = () => {
     const todoElem = document.createElement("div");
     todoElem.classList.add("todo");
     todoElem.innerText = todo.content;
+    todoElem.addEventListener("dblclick", (event) =>
+      onDblclickTodo(event, todo.id)
+    );
 
     const delBtnEle = document.createElement("button");
     delBtnEle.classList.add("delBtn");
