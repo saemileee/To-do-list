886ed57b0818506d43ce186ca0c3310e2a459b6d
saemileee <68241138+saemileee@users.noreply.github.com>
2023-02-14 01:07:57 +0900

    23-02-14 01:07
    completed btn building

--- todolist_myself/app.js
+++ todolist_myself/app.js
@@ -1,11 +1,42 @@
 const todoFormElem = document.querySelector(".todo-form");
 const todoInputElem = document.querySelector(".todo-input");
-// const todoListElem = document.querySelector(".todo-list");
 const todoListContainerElem = document.querySelector(".list-container");
 
-function onSubmitTodoForm(e) {
-  e.preventDefault();
+// class todoData {
+//   constructor(number, isCompleted, content) {
+//     this.number = 0;
+//     this.isCompleted = false;
+//     this.content = content;
+//   }
+// }
 
+// let todoDB = [];
+
+// const saveTodoData = (newTodo) => {
+//   let newTodoData = new todoData(todoDB.length, false, todoInputElem.value);
+//   todoDB.push(newTodoData);
+// };
+
+let todoData = {
+  id: 0,
+  isCompleted: false,
+  content: String,
+};
+
+let todoDB = [];
+
+const saveTodoData = (newTodo) => {
+  const newID = todoData.id + 1;
+  let newTodoData = {
+    id: newID,
+    isCompleted: false,
+    content: todoInputElem.value,
+  };
+  todoData = newTodoData;
+  todoDB.push(newTodoData);
+};
+
+const paintTodo = (newTodo) => {
   const todoListElem = document.createElement("li");
   todoListElem.classList.add("todo-list");
   todoListContainerElem.appendChild(todoListElem);
@@ -26,7 +57,20 @@ function onSubmitTodoForm(e) {
   todoListElem.appendChild(delBtnElem);
 
   todoInputElem.value = "";
+
+  completeBtnElem.addEventListener("click", onClickCompleteBtn);
+
+  function onClickCompleteBtn(e) {
+    completeBtnElem.innerText = "😍";
+    //클릭한 것의 todoData.isCompleted=true로 바뀌어야함
+  }
+  
+};
+
+function onSubmitTodoForm(e) {
+  e.preventDefault();
+  saveTodoData();
+  paintTodo();
 }
 
 todoFormElem.addEventListener("submit", onSubmitTodoForm);
-//투두리스트 인풋받아서 리스트 만들기
