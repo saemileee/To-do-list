3ed2fe95536a1ef0b54080cfaf356aa3f0d00ebf
saemileee <68241138+saemileee@users.noreply.github.com>
2023-02-13 23:29:36 +0900

    23-02-13 23:28
    onSubmitTodoForm Func

--- todolist_myself/app.js
+++ todolist_myself/app.js
@@ -1,1 +1,32 @@
-document.write("hello");
+const todoFormElem = document.querySelector(".todo-form");
+const todoInputElem = document.querySelector(".todo-input");
+// const todoListElem = document.querySelector(".todo-list");
+const todoListContainerElem = document.querySelector(".list-container");
+
+function onSubmitTodoForm(e) {
+  e.preventDefault();
+
+  const todoListElem = document.createElement("li");
+  todoListElem.classList.add("todo-list");
+  todoListContainerElem.appendChild(todoListElem);
+
+  const completeBtnElem = document.createElement("button");
+  completeBtnElem.classList.add("complete-btn");
+  completeBtnElem.innerText = "🤔";
+  todoListElem.appendChild(completeBtnElem);
+
+  const todoContentElem = document.createElement("div");
+  todoContentElem.classList.add("todo-content");
+  todoContentElem.innerText = todoInputElem.value;
+  todoListElem.appendChild(todoContentElem);
+
+  const delBtnElem = document.createElement("button");
+  delBtnElem.classList.add("del-btn");
+  delBtnElem.innerText = "❌";
+  todoListElem.appendChild(delBtnElem);
+
+  todoInputElem.value = "";
+}
+
+todoFormElem.addEventListener("submit", onSubmitTodoForm);
+//투두리스트 인풋받아서 리스트 만들기
